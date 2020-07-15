7bb4f3e2fa876c9a84552d0b56c6787f170acc49
Pratyaksh Gupta <53914703+Pratyaksh7@users.noreply.github.com>
2020-07-15 18:07:47 +0530

    polar Coordinates

---
+++ PolarCoordinates.py
@@ -0,0 +1,5 @@
+from cmath import phase
+
+z = complex(input()) # converts the input in complex number
+print(abs(z))           # abs to find the modulus of z
+print(phase(z))         # phase to find the argument of z
